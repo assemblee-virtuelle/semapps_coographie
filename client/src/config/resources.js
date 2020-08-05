9f4cf440ff77c9e13428ba3aeae16347a5a60326
simon louvet <simon.louvet.zen@gmail.com>
2020-08-05 17:11:03 +0200

    update containers

--- client/src/config/resources.js
+++ client/src/config/resources.js
@@ -1,23 +1,23 @@
 const resources = {
   Project: {
     types: ['pair:Project'],
-    containerUri: process.env.REACT_APP_MIDDLEWARE_URL + 'resources'
+    containerUri: process.env.REACT_APP_MIDDLEWARE_URL + 'projects'
   },
   Organization: {
     types: ['pair:Organization'],
-    containerUri: process.env.REACT_APP_MIDDLEWARE_URL + 'resources'
+    containerUri: process.env.REACT_APP_MIDDLEWARE_URL + 'organizations'
   },
   Person: {
     types: ['pair:Person'],
-    containerUri: process.env.REACT_APP_MIDDLEWARE_URL + 'resources'
+    containerUri: process.env.REACT_APP_MIDDLEWARE_URL + 'persons'
   },
   Thema: {
     types: ['pair:Thema'],
-    containerUri: process.env.REACT_APP_MIDDLEWARE_URL + 'resources'
+    containerUri: process.env.REACT_APP_MIDDLEWARE_URL + 'themas'
   },
   Skill: {
     types: ['pair:Skill'],
-    containerUri: process.env.REACT_APP_MIDDLEWARE_URL + 'resources'
+    containerUri: process.env.REACT_APP_MIDDLEWARE_URL + 'skills'
   },
   Agent: {
     types: ['pair:Person', 'pair:Organization']
--- server/services/ldp.service.js
+++ server/services/ldp.service.js
@@ -6,7 +6,7 @@ module.exports = {
   settings: {
     baseUrl: process.env.SEMAPPS_HOME_URL,
     ontologies,
-    containers: ['/resources'],
+    containers: ['/projects','/organizations','/persons','/themas','/skills'],
     defaultJsonContext: 'https://gist.githubusercontent.com/srosset81/cc330b63a213e6f68eb5e52d6ded4342/raw/7418e256c64685d44607f333e49489afb302a0b6/pair-ontology.json'
   }
 };
