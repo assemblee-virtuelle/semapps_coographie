d8735ca9b2f0b202ad1b73a960ef6bcc42bb4ee9
srosset81 <srosset81@gmail.com>
2020-09-03 17:03:30 +0200

    Adapt to GV file

--- server/services/ldp.service.js
+++ server/services/ldp.service.js
@@ -7,6 +7,6 @@ module.exports = {
     baseUrl: process.env.SEMAPPS_HOME_URL,
     ontologies,
     containers: ['/resources'],
-    defaultJsonContext: 'https://gist.githubusercontent.com/srosset81/140a1cfaa0f330b7c797c2ea319458bd/raw/53a36fa4e27e9405568f12e68c12e2358125ab54/gv-ontology.json'
+    defaultJsonContext: 'https://gist.githubusercontent.com/srosset81/140a1cfaa0f330b7c797c2ea319458bd/raw/ac00d92c9191468d561287985ea09a0bba22eb14/gv-ontology.json'
   }
 };
