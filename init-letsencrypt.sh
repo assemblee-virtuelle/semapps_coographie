0edd6c63853d567e07778287ead6e6446fe0a7b5
srosset81 <srosset81@gmail.com>
2020-09-03 13:03:54 +0200

    Use data.grandsvoisins.semapps.org domain for server

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(data.virtual-assembly.org)
+domains=(data.grandsvoisins.semapps.org)
 rsa_key_size=4096
 data_path="./certbot"
 email="srosset81@gmail.com"
