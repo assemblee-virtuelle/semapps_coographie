c6e2de1837009e759c722117b9ab5c2b59dd6725
srosset81 <srosset81@gmail.com>
2020-09-18 09:27:04 +0200

    Setup Redis cache

--- server/moleculer.config.js
+++ server/moleculer.config.js
@@ -1,4 +1,15 @@
 module.exports = {
   // You can set all ServiceBroker configurations here
   // See https://moleculer.services/docs/0.14/configuration.html
+  cacher: {
+    type: "Redis",
+    options: {
+      prefix: "action",
+      redis: {
+        host: "redis",
+        port: 6379,
+        db: 0
+      }
+    }
+  }
 };
