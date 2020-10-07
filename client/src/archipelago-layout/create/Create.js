88624fcd1a7a445fff3c710300f16bf7b4ca1532
srosset81 <srosset81@gmail.com>
2020-10-07 10:22:03 +0200

    Redirect to show after create

--- client/src/archipelago-layout/create/Create.js
+++ client/src/archipelago-layout/create/Create.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { Create as RaCreate } from 'react-admin';
 import CreateActions from './CreateActions';
 
-const Create = props => <RaCreate actions={<CreateActions />} {...props} />;
+const Create = props => <RaCreate actions={<CreateActions />} redirect="show" {...props} />;
 
 export default Create;
