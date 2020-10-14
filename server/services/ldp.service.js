d62c8ec2d2643ad350f6c4609a87663588a3b662
srosset81 <srosset81@gmail.com>
2020-10-14 16:27:03 +0200

    Add users container

--- server/services/ldp.service.js
+++ server/services/ldp.service.js
@@ -7,7 +7,7 @@ module.exports = {
   settings: {
     baseUrl: CONFIG.HOME_URL,
     ontologies,
-    containers: ['/organizations', '/projects', '/events', '/persons', '/themas', '/skills'],
+    containers: ['/organizations', '/projects', '/events', '/persons', '/themas', '/skills', '/users'],
     defaultJsonContext:
       'https://gist.githubusercontent.com/srosset81/cc330b63a213e6f68eb5e52d6ded4342/raw/36325e26adf8aeef948d6a3e701dff9c4c4c066b/pair-ontology.json'
   }
